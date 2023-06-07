7de131022037216f48bf61738c79b82af49e1e18
ravitejachikkala <79794643+ravitejachikkala@users.noreply.github.com>
2023-06-07 17:44:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,5 @@ module "eks_cluster" {
   cluster_version    = "1.21"  # Replace with a supported Kubernetes version
   vpc_id             = "vpc-0d6d2c6204d72763b"
   subnet_ids         = ["subnet-03ea374b93dfca015", "subnet-0f7429a6beebb71fb"]
-  create_eks_workers = false
-  map_roles          = []
-  worker_group_launch_template_config = []
+  
 }
