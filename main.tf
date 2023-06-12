4b3c4df77901a07dbba15b5ab845c6253ecca646
ravitejachikkala <79794643+ravitejachikkala@users.noreply.github.com>
2023-06-12 22:38:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 module "eks_cluster" {
   source             = "terraform-aws-modules/eks/aws"
-  cluster_name       = "eks-github-actions"
+  cluster_name       = "eks-github-cicd"
   cluster_version    = "1.26"  # Replace with a supported Kubernetes version
   vpc_id             = "vpc-0d6d2c6204d72763b"
   subnet_ids         = ["subnet-03ea374b93dfca015", "subnet-0f7429a6beebb71fb"]
